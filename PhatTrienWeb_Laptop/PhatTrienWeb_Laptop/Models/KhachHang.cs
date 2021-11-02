523794d9b7a61294cecf5977548d512b32877652
Ducvuive2 <82598608+Ducvuive2@users.noreply.github.com>
2021-11-02 11:07:40 +0700

    init model kh, sk
    init model kh, sk

---
+++ PhatTrienWeb_Laptop/PhatTrienWeb_Laptop/Models/KhachHang.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoAn_CuaHangLaptop.Models
+{
+    public class KhachHang
+    {
+        string maKH;
+        string tenKH;
+        string soDT;
+        string email;
+        string gioiTinh;
+
+        public KhachHang() { }
+        public KhachHang(string maKH, string tenKH, string soDT, string email, string gioiTinh)
+        {
+            this.MaKH = maKH;
+            this.TenKH = tenKH;
+            this.SoDT = soDT;
+            this.Email = email;
+            this.GioiTinh = gioiTinh;
+        }
+
+        public string MaKH { get => maKH; set => maKH = value; }
+        public string TenKH { get => tenKH; set => tenKH = value; }
+        public string SoDT { get => soDT; set => soDT = value; }
+        public string Email { get => email; set => email = value; }
+        public string GioiTinh { get => gioiTinh; set => gioiTinh = value; }
+    }
+}
